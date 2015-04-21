8c6256b59883601dad4fde448d17cfec939d897e
miya0001 <miya@wpist.me>
2015-04-21 13:03:04 +0900

    first commit

---
+++ nginx-css-editor.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Plugin Name: Nginx CSS Editor
+ * Plugin URI:  https://wpist.me/
+ * Description: This is a awesome cool plugin.
+ * Version:     0.1.0
+ * Author:      Takayuki Miyauchi
+ * Author URI:  https://wpist.me/
+ * License:     GPLv2
+ * Text Domain: nginx_css_editor
+ * Domain Path: /languages
+ */
+
+/**
+ * Copyright (c) 2015 Takayuki Miyauchi ( https://wpist.me/ )
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+
+define( 'NGINX_CSS_EDITOR_URL',  plugins_url( '', __FILE__ ) );
+define( 'NGINX_CSS_EDITOR_PATH', dirname( __FILE__ ) );
+
+$nginx_css_editor = new Nginx_CSS_Editor();
+$nginx_css_editor->register();
+
+class Nginx_CSS_Editor {
+
+private $version = '';
+private $langs   = '';
+
+function __construct()
+{
+    $data = get_file_data(
+        __FILE__,
+        array( 'ver' => 'Version', 'langs' => 'Domain Path' )
+    );
+    $this->version = $data['ver'];
+    $this->langs   = $data['langs'];
+}
+
+public function register()
+{
+    add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
+}
+
+public function plugins_loaded()
+{
+    load_plugin_textdomain(
+        'nginx_css_editor',
+        false,
+        dirname( plugin_basename( __FILE__ ) ).$this->langs
+    );
+
+    add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
+
+    add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+    add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+    add_action( 'admin_init', array( $this, 'admin_init' ) );
+
+}
+
+public function admin_menu()
+{
+    // See http://codex.wordpress.org/Administration_Menus
+    add_theme_page(
+        __( 'Nginx CSS Editor', 'nginx_css_editor' ),
+        __( 'Nginx CSS Editor', 'nginx_css_editor' ),
+        'manage_options', // http://codex.wordpress.org/Roles_and_Capabilities
+        'nginx_css_editor',
+        array( $this, 'options_page' )
+    );
+}
+
+public function admin_init()
+{
+    if ( isset( $_POST['_wpnonce_nginx_css_editor'] ) && $_POST['_wpnonce_nginx_css_editor'] ){
+        if ( check_admin_referer( 'nig5mycgoz2gldiign9qdue443ivn29', '_wpnonce_nginx_css_editor' ) ){
+
+            // save something
+
+            wp_safe_redirect( menu_page_url( 'nginx_css_editor', false ) );
+        }
+    }
+}
+
+public function options_page()
+{
+?>
+<div id="nginx-css-editor" class="wrap">
+<h2><?php _e( 'Nginx CSS Editor', 'nginx_css_editor' ); ?></h2>
+
+<form method="post" action="<?php echo esc_attr( $_SERVER['REQUEST_URI'] ); ?>">
+<?php wp_nonce_field( 'nig5mycgoz2gldiign9qdue443ivn29', '_wpnonce_nginx_css_editor' ); ?>
+
+<h3>PC Style</h3>
+
+<textarea name="nginx-css-editor-pc-style" class="style-editor"></textarea>
+
+<h3>SP Style</h3>
+
+<textarea name="nginx-css-editor-pc-style" class="style-editor"></textarea>
+
+<p style="margin-top: 3em;">
+    <input type="submit" name="submit" id="submit" class="button button-primary"
+            value="<?php _e( "Save Changes", "nginx_css_editor" ); ?>"></p>
+</form>
+</div><!-- #nginx_css_editor -->
+<?php
+}
+
+public function admin_enqueue_scripts( $hook )
+{
+    var_dump( $hook );
+    if ( 'appearance_page_nginx_css_editor' === $hook ) {
+        wp_enqueue_style(
+            'admin-nginx_css_editor-style',
+            plugins_url( 'css/admin-nginx-css-editor.min.css', __FILE__ ),
+            array(),
+            $this->version,
+            'all'
+        );
+
+        wp_enqueue_script(
+            'admin-nginx_css_editor-script',
+            plugins_url( 'js/admin-nginx-css-editor.min.js', __FILE__ ),
+            array( 'jquery' ),
+            $this->version,
+            true
+        );
+    }
+}
+
+public function wp_enqueue_scripts()
+{
+    wp_enqueue_style(
+        'nginx-css-editor-style',
+        plugins_url( 'css/nginx-css-editor.min.css', __FILE__ ),
+        array(),
+        $this->version,
+        'all'
+    );
+
+    wp_enqueue_script(
+        'nginx-css-editor-script',
+        plugins_url( 'js/nginx-css-editor.min.js', __FILE__ ),
+        array( 'jquery' ),
+        $this->version,
+        true
+    );
+}
+
+} // end class Nginx_CSS_Editor
+
+// EOF
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Nginx CSS Editor ===
+Contributors:      miyauchi
+Donate link:       https://wpist.me/
+Tags:
+Requires at least: 4.1.1
+Tested up to:      4.1.1
+Stable tag:        0.1.0
+License:           GPLv2
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+
+This is a awesome cool plugin.
+
+== Description ==
+
+
+
+== Installation ==
+
+= Manual Installation =
+
+1. Upload the entire `/nginx-css-editor` directory to the `/wp-content/plugins/` directory.
+2. Activate Nginx CSS Editor through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 0.1.0 =
+* First release
+
+== Upgrade Notice ==
+
+= 0.1.0 =
+First Release
